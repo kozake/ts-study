2981b7ea37d46cb371056dc6061f3250cd9bc7ca
kozake <kozake@dcr.co.jp>
2018-08-02 19:13:09 +0900

    function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,15 +1,18 @@
 "use strict";
-for (var i = 0; i < 100; i++) {
-    if (i % 15 === 0) {
-        console.log('fizzbuzz');
-    }
-    else if (i % 3 === 0) {
-        console.log('fizz');
-    }
-    else if (i % 5 === 0) {
-        console.log('buzz');
-    }
-    else {
-        console.log(i);
+function fizzbuzz(size) {
+    for (var i = 0; i < size; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        else {
+            console.log(i);
+        }
     }
 }
+fizzbuzz(300);
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,11 +1,16 @@
-for (let i = 0; i < 100; i++) {
-    if (i % 15 === 0) {
-        console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
-        console.log('fizz');
-    } else if (i % 5 === 0) {
-        console.log('buzz');
-    } else {
-        console.log(i);
+function fizzbuzz(size: number) {
+
+    for (let i = 0; i < size; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
     }
 }
+
+fizzbuzz(300);
